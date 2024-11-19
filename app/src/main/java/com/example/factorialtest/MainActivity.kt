ddf4538667ba7f1da02ef538e5956d243ebaea82
Daniil Shcherbakov <Charel@bk.ru>
2024-11-19 21:56:48 +0300

    Stateful ViewModel

--- app/src/main/java/com/example/factorialtest/MainActivity.kt
+++ app/src/main/java/com/example/factorialtest/MainActivity.kt
@@ -3,10 +3,7 @@ package com.example.factorialtest
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.factorialtest.databinding.ActivityMainBinding
 
@@ -30,26 +27,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun observeViewModel() {
-        viewModel.progress.observe(this) {
-            if (it) {
-                binding.progressBarLoading.visibility = View.VISIBLE
-                binding.buttonCalculate.isEnabled = false
-            } else {
-                binding.progressBarLoading.visibility = View.GONE
-                binding.buttonCalculate.isEnabled = true
-            }
-        }
-        viewModel.error.observe(this) {
-            if (it) {
+        viewModel.state.observe(this) {
+            if (it.isError) {
                 Toast.makeText(
                     this,
                     "You did not entered value",
                     Toast.LENGTH_SHORT
                 ).show()
             }
+            if (it.isInProgress) {
+                binding.progressBarLoading.visibility = View.VISIBLE
+                binding.buttonCalculate.isEnabled = false
+            } else {
+                binding.progressBarLoading.visibility = View.GONE
+                binding.buttonCalculate.isEnabled = true
+            }
+            binding.textViewFactorial.text = it.factorial
         }
-        viewModel.factorial.observe(this) {
-            binding.textViewFactorial.text = it
-        }
+
     }
 }
