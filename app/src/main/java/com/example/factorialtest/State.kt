f6744d91f5eea9dabd2fc57299ab81f847eb7aff
Daniil Shcherbakov <Charel@bk.ru>
2024-11-20 12:22:00 +0300

    Use withContext and suspendCoroutine

--- app/src/main/java/com/example/factorialtest/State.kt
+++ app/src/main/java/com/example/factorialtest/State.kt
@@ -3,4 +3,4 @@ package com.example.factorialtest
 sealed class State
 object Error : State()
 object Progress : State()
-class Result(val factorial: String = "") : State()
+class Factorial(val value: String = "") : State()
